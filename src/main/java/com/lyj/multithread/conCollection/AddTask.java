3e1142076a11e5aa2d178959a98730cedc350fa3
ja0ck5 <975117619@qq.com>
2017-03-20 18:35:41 +0800

    ConcurrentLinkedDeque

---
+++ src/main/java/com/lyj/multithread/conCollection/AddTask.java
@@ -0,0 +1,15 @@
+package com.lyj.multithread.conCollection;
+
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+public class AddTask implements Runnable {
+	
+	private ConcurrentLinkedDeque<String> list;
+
+
+	@Override
+	public void run() {
+
+	}
+
+}
