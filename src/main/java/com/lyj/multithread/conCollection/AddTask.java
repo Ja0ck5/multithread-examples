3cc20b6baca1a2721e716f8fad8cc62892ed06b5
ja0ck5 <975117619@qq.com>
2017-03-20 18:38:23 +0800

    run

--- src/main/java/com/lyj/multithread/conCollection/AddTask.java
+++ src/main/java/com/lyj/multithread/conCollection/AddTask.java
@@ -3,12 +3,20 @@ package com.lyj.multithread.conCollection;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class AddTask implements Runnable {
-	
+
 	private ConcurrentLinkedDeque<String> list;
+
+	public AddTask(ConcurrentLinkedDeque<String> list) {
+		super();
+		this.list = list;
+	}
 
 	@Override
 	public void run() {
-
+		String name = Thread.currentThread().getName();
+		for (int i = 0; i < 10000; i++) {
+			list.add(name + ": Element " + i);
+		}
 	}
 
 }
