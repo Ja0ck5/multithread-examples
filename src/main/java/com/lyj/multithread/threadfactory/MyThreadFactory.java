bfc93f42ab453fb3d0b217e3d7fdda242988f439
ja0ck5 <975117619@qq.com>
2017-03-23 16:37:50 +0800

    threadfactory

---
+++ src/main/java/com/lyj/multithread/threadfactory/MyThreadFactory.java
@@ -0,0 +1,12 @@
+package com.lyj.multithread.threadfactory;
+
+import java.util.concurrent.ThreadFactory;
+
+public class MyThreadFactory implements ThreadFactory {
+
+	@Override
+	public Thread newThread(Runnable r) {
+		return null;
+	}
+
+}
