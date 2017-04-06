115456942488611dbac8e02d62657a56d1c916bb
ja0ck5 <975117619@qq.com>
2017-04-06 08:26:01 +0800

    变量需要显示置空的情况

--- src/test/java/com/lyj/boundedbuffer/BoundedBufferTest.java
+++ src/test/java/com/lyj/boundedbuffer/BoundedBufferTest.java
@@ -54,7 +54,8 @@ public class BoundedBufferTest extends TestCase {
 	 * 将多个大型对象插入到一个有界缓存中，然后再将它们移除
 	 * 第2个堆快照中的内存用量应该与第一个对快照中的内存用量基本相同。
 	 * 然而如果 doExtract 忘记将返回元素的索引置空(items[i]=null)
-	 * 那么两次快照的内存用量明显不同，这就是为什么需要显示地将变量置空的情况
+	 * 那么两次快照的内存用量明显不同，这就是为什么需要显示地将变量置空的情况。
+	 * 但在大多数情况下，这种情况不仅不会打来帮助，反而会带来负面作用。
 	 * @throws InterruptedException
 	 */
 	void testLeak() throws InterruptedException{
